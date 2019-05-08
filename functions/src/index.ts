813ba0130bd96b2cd53332de28efce11f722d49c
Marco Tulio Teixeira <omarcoteixeira@gmail.com>
2019-05-08 11:42:14 -0300

    Add support to get previous data.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,50 @@
 import * as functions from 'firebase-functions';
 import * as request from "request-promise-native";
 import { Firestore } from '@google-cloud/firestore';
+import { Draw } from './models/Draw';
 
 const firestore = new Firestore();
 
-const parseDrawData = (draw: any) => {
-    return {
-        number: draw.numero,
-        date: draw.data,
-        numbers: draw.sorteio,
-        accumulated: draw.acumulado,
-        accumulatedValue: draw.valor_acumulado,
-        extimatedValue: draw.proximo_estimativa,
-        nextDate: draw.proximo_data
-    }
+const getDraw = async (data: any, draw?: number) => {
+    const drawNumber = !!draw ? draw : '';
+    const url = `https://www.lotodicas.com.br/api/${data.type}/${drawNumber}`;
+
+    const raw = await request.get(url);
+    return Draw.fromJson(raw);
 };
 
-export const crawlers = functions.pubsub.topic('crawlers').onPublish(async (message: any) => {
+const saveDraw = (data: any, draw: Draw) => {
+    return firestore
+        .collection(data.type)
+        .doc(draw.number.toString())
+        .set(JSON.parse(JSON.stringify(draw)), { merge: true });
+};
+
+const saveCurrentDraw = async (data: any) => {
+    const draw = await getDraw(data);
+
+    await saveDraw(data, draw);
+    return draw;
+}
+
+const savePreviousDraw = async (data: any, draw: Draw) => {
+    const previousDraw = await getDraw(
+        data, 
+        parseInt(draw.number) - 1
+    );
+
+    return saveDraw(data, previousDraw);
+}
+
+export const crawlers = functions
+    .pubsub
+    .topic('crawlers')
+    .onPublish(async (message: any) => {
 
     const data = JSON.parse(
         Buffer.from(message.data, 'base64').toString()
     );
-
-    const url = `https://www.lotodicas.com.br/api/${data.type}`;
-    const raw = JSON.parse(
-        await request.get(url)
-    );
-
-    const result = parseDrawData(raw);
-    return firestore
-        .collection(`draws`)
-        .doc(data.type)
-        .collection(result.number.toString())
-        .add(result);
+    
+    const draw = await saveCurrentDraw(data);
+    return savePreviousDraw(data, draw);
 });
