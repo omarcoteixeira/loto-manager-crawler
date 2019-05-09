ed36be8a22943cbddd7f73a8ac6cea8caa41864a
Marco Tulio Teixeira <omarcoteixeira@gmail.com>
2019-05-09 11:07:50 -0300

    Add comments to the functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,11 @@ import { Draw } from './model/Draw';
 
 const firestore = new Firestore();
 
+/**
+ * Get details from a draw
+ * @param data Data retrieved from pub/sub
+ * @param draw (Optional) Retrieve data from a specific draw
+ */
 const getDraw = async (data: any, draw?: number) => {
     const drawNumber = !!draw ? draw : '';
     const url = `https://www.lotodicas.com.br/api/${data.type}/${drawNumber}`;
@@ -13,6 +18,11 @@ const getDraw = async (data: any, draw?: number) => {
     return Draw.fromJson(raw);
 };
 
+/**
+ * Save draw details on firestore
+ * @param data Data retrieved from pub/sub
+ * @param draw Draw to be saved
+ */
 const saveDraw = (data: any, draw: Draw) => {
     return firestore
         .collection(data.type)
@@ -20,6 +30,10 @@ const saveDraw = (data: any, draw: Draw) => {
         .set(JSON.parse(JSON.stringify(draw)), { merge: true });
 };
 
+/**
+ * Get a current draw and save on firestore
+ * @param data Data retrieved from pub/sub
+ */
 const saveCurrentDraw = async (data: any) => {
     const draw = await getDraw(data);
 
@@ -27,6 +41,11 @@ const saveCurrentDraw = async (data: any) => {
     return draw;
 }
 
+/**
+ * Get a previous draw based on the current draw and save it on firestore.
+ * @param data Data retrieved from pub/sub
+ * @param currentDraw Current Draw used to find the previous one
+ */
 const savePreviousDraw = async (data: any, currentDraw: Draw) => {
     const previousDraw = await getDraw(
         data, 
@@ -36,15 +55,20 @@ const savePreviousDraw = async (data: any, currentDraw: Draw) => {
     return saveDraw(data, previousDraw);
 }
 
+/**
+ * Trigger for crawlers pub/sub.
+ */
 export const crawlers = functions
     .pubsub
     .topic('crawlers')
     .onPublish(async (message: any) => {
 
+    // Parse data retrieved from Pub/Sub
     const data = JSON.parse(
         Buffer.from(message.data, 'base64').toString()
     );
     
+    // Save the current draw and use the returned object to find/save the previous one.
     const currentDraw = await saveCurrentDraw(data);
     return savePreviousDraw(data, currentDraw);
 });
